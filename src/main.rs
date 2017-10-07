9ef67fccd19b62f2d094125e82ac456524503a5c
knknkn1162 <knknkn1162@gmail.com>
2017-10-07 16:54:05 +0900

    add test Q03 in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod ch01;
 fn main() {
     assert_eq!("desserts", ch01::reverse("stressed"));
     assert_eq!("パトカー", ch01::extract("パタトクカシーー", |idx| idx%2==0));
-    assert_eq!("パタトクカシーー", ch01::join_alt("パトカー", "タクシー"))
+    assert_eq!("パタトクカシーー", ch01::join_alt("パトカー", "タクシー"));
+    assert_eq!(vec![3,1,4,1,6,9,2,7,5,3,5,8,9,7,0], ch01::convert_piem());
 }
 
