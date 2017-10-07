d00ad0fdf15551ddda3a7cef19c5555c42ef94f4
knknkn1162 <knknkn1162@gmail.com>
2017-10-07 16:34:59 +0900

    test Q.02

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ mod ch01;
 fn main() {
     assert_eq!("desserts", ch01::reverse("stressed"));
     assert_eq!("パトカー", ch01::extract("パタトクカシーー", |idx| idx%2==0));
-
+    assert_eq!("パタトクカシーー", ch01::join_alt("パトカー", "タクシー"))
 }
+
