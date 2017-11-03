e51c252465c92b54df787f1c57365e58b9d9b32d
knknkn1162 <knknkn1162@gmail.com>
2017-11-03 21:14:48 +0900

    impl FileExtractor

---
+++ src/ch02/ch02.rs
@@ -0,0 +1,44 @@
+use std::io::{BufReader, Read};
+use std::fs::File;
+use std::io;
+
+struct FileExtractor {path: String}
+
+impl FileExtractor {
+    pub fn new(path: String)-> FileExtractor {
+        FileExtractor {path: path}
+    }
+
+    fn read(&self)->Result<String,io::Error>  {
+        let f = File::open(&self.path)?; // read only.
+        let mut reader = BufReader::new(f);
+
+        let mut line = String::new();
+        let len = reader.read_to_string(&mut line)?;
+
+        debug!("{} characters", len);
+        Ok(line)
+    }
+
+    pub fn wc(&self)->usize {
+        let s = self.read().unwrap();
+        s.lines().collect::<Vec<_>>().len()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use ch02::command;
+    use super::FileExtractor;
+    #[test]
+    fn test_read() {
+        let fext = FileExtractor {path: "./data/ch02/hightemp.txt".to_owned()};
+
+        let res = fext.read().unwrap();
+
+        assert_eq!(
+            res.lines().take(1).collect::<String>(),
+            "高知県\t江川崎\t41\t2013-08-12"
+        );
+    }
+}
