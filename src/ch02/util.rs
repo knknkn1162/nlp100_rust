6317b2392a96dd45ef85b97e3c73e6bc4044e9fa
knknkn1162 <knknkn1162@gmail.com>
2017-11-05 21:22:22 +0900

    impl trim_mut & test in util module

--- src/ch02/util.rs
+++ src/ch02/util.rs
@@ -4,6 +4,15 @@ pub fn get_split_line_count(size: usize, split_num: usize)->usize {
     if size%split_num==0 {res} else {res+1}
 }
 
+/// more efficient trim than String::trim that signature is Fn(String)->&str
+pub fn trim_mut(s: &mut String, ch: char) {
+    let mut len = s.len();
+    while s.ends_with(ch) {
+        len -= 1;
+        s.truncate(len);
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -14,5 +23,13 @@ mod tests {
         assert_eq!(get_split_line_count(9, 3), 3);
         assert_eq!(get_split_line_count(10, 3), 4);
         assert_eq!(get_split_line_count(11, 3), 4);
+    }
+
+    #[test]
+    fn test_trim_mut() {
+        let mut s = "abc\n\n".to_string();
+        trim_mut(&mut s, '\n');
+
+        assert_eq!("abc", s);
     }
 }
