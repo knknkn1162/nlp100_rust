dfcd9a4df3fccd33782e6e3caaa1edc6c0ee76e9
knknkn1162 <knknkn1162@gmail.com>
2017-11-13 22:51:38 +0900

    extern crate serde, csv and chrono

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 //! an `extern crate` loading macros must be at the crate root
 #[macro_use(log, debug, info)]
 extern crate log;
-
+#[macro_use]
+extern crate serde_derive;
 
 pub mod ch01;
 pub mod ch02;
