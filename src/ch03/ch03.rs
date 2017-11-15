1b956df8e0a185cf2e53d2b96022ad38ac561219
knknkn1162 <knknkn1162@gmail.com>
2017-11-15 22:03:00 +0900

    impl download method in ch03

--- src/ch03/ch03.rs
+++ src/ch03/ch03.rs
@@ -1,10 +1,11 @@
 extern crate reqwest;
-use std::io::{Read, copy};
+use std::io::{Read, copy, Result as ioResult};
 use std::path::Path;
 use std::fs::File;
+use std::process::{Command, Stdio};
 
 
-fn download<T: reqwest::IntoUrl, P: AsRef<Path>>(url: T, save_dir: P) {
+fn download<T: reqwest::IntoUrl, P: AsRef<Path>>(url: T, save_dir: P)->ioResult<()> {
     let mut response = reqwest::get(url).unwrap();
     let fname = {
         let fname = response.url()
@@ -15,8 +16,19 @@ fn download<T: reqwest::IntoUrl, P: AsRef<Path>>(url: T, save_dir: P) {
         save_dir.as_ref().join(fname)
     };
 
-    let _ = ::std::fs::create_dir(save_dir).unwrap();
-    copy(&mut response, &mut File::create(fname).unwrap()).unwrap();
+    let _ = ::std::fs::create_dir(save_dir); // ignore save_dir exists
+    let mut f = File::create(&fname)?;
+    copy(&mut response, &mut f);
+
+    let output = Command::new("gunzip")
+        .arg(fname)
+        .output()?;
+
+    eprintln!("status: {:?}", output.status);
+    eprintln!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    eprintln!("stderr: \n{}", String::from_utf8_lossy(&output.stderr));
+    Ok(())
+
 }
 
 #[cfg(test)]
